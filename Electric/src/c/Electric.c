e1486cbc07cc6557a2fbf08564735f57ef65c329
Stefan Bauwens <stefan.bauwens@hotmail.com>
2022-11-19 22:35:51 +0000

    Add background + make time work

--- Electric/src/c/Electric.c
+++ Electric/src/c/Electric.c
@@ -1,13 +1,73 @@
 #include <pebble.h>
 
 static Window *s_main_window;
+static TextLayer *s_time_layer;
+static BitmapLayer *s_bitmap_layer;
+static GFont s_time_font;
+static GBitmap *s_bitmap;
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H%M" : "%I%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
 
 static void main_window_load(Window *window) {
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
 
+  // Create GBitmap for background
+  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BG_IMAGE);
+
+  // Create bitmap layer
+  s_bitmap_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
+  bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
+
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PIXEL_LCD_40));
+
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(
+      GRect(-8, 5, bounds.size.w, 50));
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBrightGreen);
+  //text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
 
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
+
+  // Destroy bitmap & layer
+  gbitmap_destroy(s_bitmap);
+  bitmap_layer_destroy(s_bitmap_layer);
 }
 
 static void init() {
@@ -22,6 +82,12 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+  // Make sure the time is displayed from the start
+  update_time();
 }
 
 static void deinit() {
