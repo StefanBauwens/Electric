2d90266d73d0d2ac95678251d4e0a74d1ffae523
Stefan Bauwens <stefan.bauwens@hotmail.com>
2022-11-20 15:40:53 +0000

    Add function to set led matrix in code

--- Electric/src/c/Electric.c
+++ Electric/src/c/Electric.c
@@ -3,10 +3,35 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_lcd_layer; //16x2 lcd
+static TextLayer *s_matrix_layer;
 static BitmapLayer *s_bitmap_layer;
 static GFont s_time_font;
 static GFont s_lcd_font;
+static GFont s_matrix_font;
 static GBitmap *s_bitmap;
+
+static void set_matrix(char *row1, char *row2, char *row3, char *row4, char *row5, char *row6, char *row7, char *row8)
+{
+  static char s_matrix_buffer[75];
+  strcat(s_matrix_buffer, row1);
+  strcat(s_matrix_buffer, " "); //extra space is neccesary to add breaks between "words" to prevent bad wrapping
+  strcat(s_matrix_buffer, row2);
+  strcat(s_matrix_buffer, " ");
+  strcat(s_matrix_buffer, row3);
+  strcat(s_matrix_buffer, " ");
+  strcat(s_matrix_buffer, row4);
+  strcat(s_matrix_buffer, " ");
+  strcat(s_matrix_buffer, row5);
+  strcat(s_matrix_buffer, " ");
+  strcat(s_matrix_buffer, row6);
+  strcat(s_matrix_buffer, " ");
+  strcat(s_matrix_buffer, row7);
+  strcat(s_matrix_buffer, " ");
+  strcat(s_matrix_buffer, row8);
+  strcat(s_matrix_buffer, " ");
+
+  text_layer_set_text(s_matrix_layer, s_matrix_buffer);
+}
 
 static void update_time() {
   // Get a tm structure
@@ -48,17 +73,18 @@ static void main_window_load(Window *window) {
   layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
 
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PIXEL_LCD_40));
-  s_lcd_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SMALL_LCD_8));
+  s_time_font   = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PIXEL_LCD_40));
+  s_lcd_font    = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SMALL_LCD_8));
+  s_matrix_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_LEDMATRIX_6));
 
   // Create the TextLayers with specific bounds
-  s_time_layer = text_layer_create(GRect(-8, 5, bounds.size.w, 50));
-  s_lcd_layer  = text_layer_create(GRect(30, 76, 96, 50));
+  s_time_layer   = text_layer_create(GRect(-8, 5, bounds.size.w, 50));
+  s_lcd_layer    = text_layer_create(GRect(30, 76, 96, 50));
+  s_matrix_layer = text_layer_create(GRect(6, 113, 50, 50));
 
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBrightGreen);
-  //text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
@@ -70,9 +96,25 @@ static void main_window_load(Window *window) {
   text_layer_set_overflow_mode(s_lcd_layer, GTextOverflowModeWordWrap);
   //text_layer_set_text(s_lcd_layer, "Sunday 20 Nov   7.22'C"); //TODO test
 
+  //handle matrix text layer
+  text_layer_set_background_color(s_matrix_layer, GColorClear);
+  text_layer_set_text_color(s_matrix_layer, GColorBrightGreen);
+  text_layer_set_font(s_matrix_layer, s_matrix_font);
+  text_layer_set_text_alignment(s_matrix_layer, GTextAlignmentLeft);
+  text_layer_set_overflow_mode(s_matrix_layer, GTextOverflowModeWordWrap);
+  set_matrix("        ", 
+             " 11  11 ", 
+             "11111111",
+             "11111111",
+             "11111111",
+             " 111111 ",
+             "  1111  ",
+             "   11   ");
+
   // Add Text as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_lcd_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_matrix_layer));
 }
 
 static void main_window_unload(Window *window) {
